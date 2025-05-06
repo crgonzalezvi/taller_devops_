a178788d77b7da8ab1c165e0281c642cd366355d
crgonzalezvi <crgonzalezvi@unal.edu.co>
2025-05-06 18:11:11 -0500

    Calcular y mostrar promedio general de notas

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from estudiantes.registro import cargar_estudiantes, mostrar_tabla_estudiantes
+from estudiantes.registro import (
+    cargar_estudiantes,
+    mostrar_tabla_estudiantes,
+    calcular_promedio
+)
+
 
 def main():
     estudiantes = cargar_estudiantes('estudiantes.csv')
     mostrar_tabla_estudiantes(estudiantes)
+    calcular_promedio(estudiantes)
 
 if __name__ == "__main__":
     main()
